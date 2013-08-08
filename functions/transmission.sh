a473e5c2d8f3e1bac5386ab7789df31fe959dffc
James Shield <shemlesh@gmail.com>
2013-08-08 20:09:27 +0800

    Added extra torrent commands

--- functions/transmission.sh
+++ functions/transmission.sh
@@ -3,4 +3,23 @@ function tc() { transmission-remote $TRANSHOST "$@"
 }
 
 function tl() { tc -l 
+}__addtorrent () 
+{ 
+    if test -z $1; then
+        return 1;
+    fi;
+    if test -z $2; then
+        return 2;
+    fi;
+    tc -w "$2" -a "$1"
+}
+__addvideotorrent () 
+{ 
+    if test -z $1; then
+        return 1;
+    fi;
+    if test -z $2; then
+        return 2;
+    fi;
+    __addtorrent "$1" "/srv/internal/videos/$2"
 }
