4514edeea6b286da32ab929f85070269ff18a334
James Shield <shemlesh@gmail.com>
2013-08-08 20:30:17 +0800

    Added function to create a folder under transmission basepath

--- functions/transmission.sh
+++ functions/transmission.sh
@@ -15,6 +15,7 @@ __addtorrent ()
     fi;
     tc -w "$2" -a "$1"
 }
+
 __addvideotorrent () 
 { 
     if test -z $1; then
@@ -24,4 +25,16 @@ __addvideotorrent ()
         return 2;
     fi;
     __addtorrent "$1" "/srv/internal/videos/$2"
+}
+
+__transmission_mkdir () 
+{ 
+    local BASEPATH="/srv/internal/";
+    local NEWPATH="$BASEPATH/$1";
+    if test -d $NEWPATH; then
+        echo "Aborting as directory already exists";
+        return 1;
+    fi;
+    mkdir -p -p $NEWPATH;
+    chown transmission -R $NEWPATH
 }
